b40b8d743c549eb8fa5770daff5fa621a89060b1
pmeig <pierrick.workplace@gmail.com>
2025-04-27 21:08:41 +0200

    add breadcrumb

---
+++ projects/bootstrap/components/breadcrumb/src/lib/b-breadcrumb.module.ts
@@ -0,0 +1,10 @@
+import { BBreadcrumbDirective } from './b-breadcrumb.directive';
+import { NgModule } from '@angular/core';
+
+const BREADCRUMB_MODULE = [BBreadcrumbDirective];
+
+@NgModule({
+  imports: BREADCRUMB_MODULE,
+  exports: BREADCRUMB_MODULE
+})
+export class BreadcrumbMaterial {}
---
+++ projects/bootstrap/components/breadcrumb/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of b-breadcrumb
+ */
+
+export * from './lib/b-breadcrumb.directive'
+export * from './lib/b-breadcrumb.module'
